2a60e8f241d1d84176b3a8f902bbc44dfd9e60fb
niteshseram <niteshseram@gmail.com>
2020-10-11 22:02:20 +0530

    Updates

---
+++ precache-manifest.15f9056a30da7f9040da64ff1e840940.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd8ea573422c64b0575f85182cd3d1d",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "592cf97eda4e42b4a3e5",
+    "url": "/covid19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "aef47f1147bc268c4b9b",
+    "url": "/covid19-tracker/static/css/main.f724bc26.chunk.css"
+  },
+  {
+    "revision": "592cf97eda4e42b4a3e5",
+    "url": "/covid19-tracker/static/js/2.ce75e30e.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid19-tracker/static/js/2.ce75e30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef47f1147bc268c4b9b",
+    "url": "/covid19-tracker/static/js/main.0bd97247.chunk.js"
+  },
+  {
+    "revision": "6b444aef87d753a0bf04",
+    "url": "/covid19-tracker/static/js/runtime-main.815270a9.js"
+  }
+]);
